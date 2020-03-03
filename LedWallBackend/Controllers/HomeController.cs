214717a81a17a79c707321189f8ee37dcdca6b6c
Simon Heiss <simonheiss87@gmail.com>
2020-03-04 00:09:33 +0100

    less shit

--- LedWallBackend/Controllers/HomeController.cs
+++ LedWallBackend/Controllers/HomeController.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using LedWallBackend.Models;
 
 namespace LedWallBackend.Controllers
 {
