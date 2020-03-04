75a64c704decd9f26d22a4457467ac59d500d46b
Simon Heiss <simonheiss87@gmail.com>
2020-03-04 16:10:40 +0100

    extracted functions

--- LedWallBackend/Controllers/HomeController.cs
+++ LedWallBackend/Controllers/HomeController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,26 +22,35 @@ namespace LedWallBackend.Controllers
 
             var bmp = new Bitmap(memoryStream);
 
-            using (Bitmap resized = new Bitmap(400, 600, bmp.PixelFormat))
-            {
-                using var g = Graphics.FromImage(resized);
-                g.DrawImage(bmp, new Rectangle(Point.Empty, resized.Size));
-                var bitmapResized = new Bitmap(400, 600, g);
-
-                var matrix = new Color[bitmapResized.Width][];
-                for (var i = 0; i <= bitmapResized.Width - 1; i++)
-                {
-                    matrix[i] = new Color[bitmapResized.Height];
-                    for (var j = 0; j < bitmapResized.Height - 1; j++)
-                    {
-                        matrix[i][j] = bitmapResized.GetPixel(i, j);
-                    }
-                }
-            }
+            var bitmapResized = ResizeBmp(bmp, 400, 600);
+            MapToColorDto(bitmapResized);
 
             memoryStream.Close();
 
             return Redirect("/");
+        }
+
+        private static Bitmap ResizeBmp(Bitmap bmp, int x, int y)
+        {
+            return bmp;
+            // var resized = new Bitmap(x, y, bmp.PixelFormat);
+            // using var g = Graphics.FromImage(resized);
+            // g.DrawImage(bmp, new Rectangle(Point.Empty, resized.Size));
+            // var bitmapResized = new Bitmap(x, y, g);
+            // return bitmapResized;
+        }
+
+        private static void MapToColorDto(Bitmap bitmap)
+        {
+            var matrix = new Color[bitmap.Width][];
+            for (var i = 0; i <= bitmap.Width - 1; i++)
+            {
+                matrix[i] = new Color[bitmap.Height];
+                for (var j = 0; j < bitmap.Height - 1; j++)
+                {
+                    matrix[i][j] = bitmap.GetPixel(i, j);
+                }
+            }
         }
     }
 
