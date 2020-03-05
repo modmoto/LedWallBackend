0c047be279a9f7dae5770596d5fda4ab11492d7b
Simon Heiss <simonheiss87@gmail.com>
2020-03-05 17:50:50 +0100

    see what that does

--- LedWallBackend/wwwroot/js/site.js
+++ LedWallBackend/wwwroot/js/site.js
@@ -21,34 +21,6 @@ const colorPicker = new iro.ColorPicker('#color-picker-container', {
 
 const cpContainer = document.getElementById('color-picker-container');
 
-$(window).bind('orientationchange resize', function(event){
-    if (event.orientation) {
-        if (event.orientation == 'landscape') {
-            if (window.rotation == 90) {
-                rotate(this, -90);
-            } else {
-                rotate(this, 90);
-            }
-        }
-    }
-});
-
-function rotate(el, degs) {
-    iedegs = degs/90;
-    if (iedegs < 0) iedegs += 4;
-    transform = 'rotate('+degs+'deg)';
-    iefilter = 'progid:DXImageTransform.Microsoft.BasicImage(rotation='+iedegs+')';
-    styles = {
-        transform: transform,
-        '-webkit-transform': transform,
-        '-moz-transform': transform,
-        '-o-transform': transform,
-        filter: iefilter,
-        '-ms-filter': iefilter
-    };
-    $(el).css(styles);
-}
-
 window.addEventListener('load', function () {
     const drawer = {
         isDrawing: false,
