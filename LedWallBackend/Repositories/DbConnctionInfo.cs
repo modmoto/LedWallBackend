379a0b6324a1b4d3c3c4c7451e9aeca37c74c6da
Simon Heiss <simonheiss87@gmail.com>
2020-03-05 13:13:28 +0100

    jeah man

--- LedWallBackend/Repositories/DbConnctionInfo.cs
+++ LedWallBackend/Repositories/DbConnctionInfo.cs
@@ -4,7 +4,7 @@ namespace LedWallBackend.Repositories
     {
         public DbConnctionInfo(string connectionString)
         {
-            ConnectionString = !string.IsNullOrEmpty(connectionString) ? connectionString : "mongodb://localhost:27017/";
+            ConnectionString = !string.IsNullOrEmpty(connectionString) ? connectionString : "mongodb://h2865571.stratoserver.net:4001/?readPreference=primary&ssl=false";
         }
 
         public string ConnectionString { get; }
